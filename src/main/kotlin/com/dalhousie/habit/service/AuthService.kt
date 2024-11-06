89270527637f25b982029f55b7dee36df71eee3d
Vraj Shah <shahvraj2016@gmail.com>
2024-11-06 00:35:29 -0400

    feat: Add login API to log in user to the application

--- src/main/kotlin/com/dalhousie/habit/service/AuthService.kt
+++ src/main/kotlin/com/dalhousie/habit/service/AuthService.kt
@@ -1,8 +1,13 @@
 package com.dalhousie.habit.service
 
+import com.dalhousie.habit.exception.InvalidPasswordException
 import com.dalhousie.habit.exception.UserAlreadyRegisteredException
+import com.dalhousie.habit.exception.UserNotFoundException
 import com.dalhousie.habit.repository.UserRepository
+import com.dalhousie.habit.request.LoginRequest
 import com.dalhousie.habit.request.RegisterRequest
+import com.dalhousie.habit.response.LoginResponse
+import com.dalhousie.habit.response.LoginResponse.Data
 import com.dalhousie.habit.response.RegisterResponse
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -15,12 +20,20 @@ interface AuthService {
      * @return [RegisterResponse] object
      */
     fun registerUser(request: RegisterRequest): RegisterResponse
+
+    /**
+     * Login the user into the application
+     * @param request User details to login
+     * @return [LoginResponse] object
+     */
+    fun loginUser(request: LoginRequest): LoginResponse
 }
 
 @Service
 class AuthServiceImpl(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtService: JwtService
 ) : AuthService {
 
     override fun registerUser(request: RegisterRequest): RegisterResponse {
@@ -30,5 +43,15 @@ class AuthServiceImpl(
         val user = request.toUser(passwordEncoder)
         userRepository.save(user)
         return RegisterResponse.success()
+    }
+
+    override fun loginUser(request: LoginRequest): LoginResponse {
+        val user = userRepository.findByEmail(request.email)
+            ?: throw UserNotFoundException(request.email)
+        if (!passwordEncoder.matches(request.password, user.password))
+            throw InvalidPasswordException(request.email)
+
+        val token = jwtService.generateToken(user)
+        return LoginResponse.success(Data(token))
     }
 }
--- src/main/kotlin/com/dalhousie/habit/util/Constants.kt
+++ src/main/kotlin/com/dalhousie/habit/util/Constants.kt
@@ -3,4 +3,5 @@ package com.dalhousie.habit.util
 object Constants {
     const val GENERIC_API_ERROR_MESSAGE = "An unexpected error occurred."
     const val REGISTER_REQUEST_SUCCESS = "User registered successfully."
+    const val LOGIN_REQUEST_SUCCESS = "User login successful."
 }
